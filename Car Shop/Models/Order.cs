29dfd133d69f8c2cf2beecdbba0ba70dce784710
Altay Zhenis <Altayaka47@gmail.com>
2020-04-02 16:33:16 +0600

    Добавьте файлы проекта.

---
+++ Car Shop/Models/Order.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Car_Shop.Models
+{
+    public class Order
+    {
+        public int Id { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public int CustomerId { get; set; }
+        public Customer Customer { get; set; }
+        public string NameOnCard { get; set; }
+        public string Adress { get; set; }
+        public string PhoneNum { get; set; }
+        public List<OrderDetail> OrderDetails { get; set; }
+    }
+}
---
+++ Car Shop/Models/Product.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Car_Shop.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string ImageUrl { get; set; }
+        public string ShortDescription { get; set; }
+        public string LongDescription { get; set; }
+        public int CategoryId { get; set; }
+        public Category Category { get; set; }
+
+    }
+}
